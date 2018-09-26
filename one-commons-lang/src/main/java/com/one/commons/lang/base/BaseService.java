3185e4849efebf84fe00632e730a0bbeff5d568e
wengxiaoliang <weng.weng@aeasycredit.com>
2018-09-26 10:11:01 +0800

    Add 添加dto/model/service基类及ObjectsUtils

---
+++ one-commons-lang/src/main/java/com/one/commons/lang/base/BaseService.java
@@ -0,0 +1,24 @@
+/*
+ * 描述： Base Service
+ * 修改人： Weng.weng
+ * 修改时间： Sep 26, 2018
+ * 项目： one-commons-lang
+ */
+package com.one.commons.lang.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Base Service<br>
+ * 
+ * @author Weng.weng
+ * @version [版本号, Sep 26, 2018]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+public class BaseService {
+    
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+    
+}
